6a9279487c217448efac652cdc25f0f3145634a8
Your Name <you@example.com>
2021-12-28 16:37:56 +0400

    fix: update tests

--- libs/rucken/src/lib/rucken.spec.ts
+++ libs/rucken/src/lib/rucken.spec.ts
@@ -3,7 +3,7 @@ import { rucken } from './rucken';
 describe('rucken', () => {
   it('should work', () => {
     expect(rucken()).toEqual(
-      'sorry but i start write new version of rucken, for use prev version please change version to "1.13.3"'
+      'Sorry but i start write new version of rucken, for use prev version please change version to "npm i --save-dev rucken@1.13.3"'
     );
   });
 });
