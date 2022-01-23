170b7b3dc9939e63881001a66c032e154e23dbb2
EndyKaufman <admin@site15.ru>
2022-01-23 21:36:21 +0400

    chore(tools): add e2e tests

---
+++ integrations/scripts/prepare-application.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+rm -rf ./integrations/app
+mkdir ./integrations/app
+cd ./integrations
+npx --yes create-nx-workspace --name=app --preset=empty --interactive=false --nx-cloud=false
+cd app
+npm install --save-dev @nrwl/angular @nrwl/nest @nrwl/node --force
+npx --yes nx g @nrwl/nest:app server
+npx --yes nx g @nrwl/angular:app client --style=scss --routing=true
+npx --yes nx g @nrwl/node:app cli
+npx --yes nx g @nrwl/nest:lib feature/server
+npx --yes nx g @nrwl/angular:lib feature/client
+npx --yes nx g @nrwl/node:lib feature/common
+cd ../../
+cp -Rf ./integrations/default/* ./integrations/app
+mkdir ./integrations/app/lib
+cp -Rf ./dist/libs/rucken/* ./integrations/app/lib
+npx --yes replace-json-property ./integrations/app/lib/package.json version 0.0.0
+cd ./integrations/app/lib
+npm pack .
+cd ../
+npx --yes nx g @nrwl/workspace:remove client-e2e
+npx --yes npm-check-updates -u
+npm i --force
+npm install --save-dev ./lib/rucken-0.0.0.tgz --force
+npx rucken tools all
+npx nx format
+tsc --noEmit -p tsconfig.base.json
+npx --yes nx affected:build --all
+
+# mkdir ./integrations/app/src/app/new-api
+# cp -Rf ./apps/demo/src/app/panels/new-api/* ./integrations/app/src/app/new-api/
+# cp -Rf ./integrations/default/* ./integrations/app/src/app/
+# node ./integrations/scripts/path-angular-files.js
+# npm run build -- --prod
+cd ../../
---
+++ libs/rucken/tests/e2e/tools-version-updater.spec.ts
@@ -0,0 +1,154 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
+describe('Tools versionUpdater (e2e)', () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let rootJson: any;
+
+  beforeAll(() => {
+    rootJson = JSON.parse(
+      readFileSync(
+        resolve(
+          __dirname,
+          '..',
+          '..',
+          '..',
+          '..',
+          'integrations/app/package.json'
+        )
+      ).toString()
+    );
+  });
+
+  it('libs/feature-client', () => {
+    const json = JSON.parse(
+      readFileSync(
+        resolve(
+          __dirname,
+          '..',
+          '..',
+          '..',
+          '..',
+          'integrations/app/libs/feature/client/package.json'
+        )
+      ).toString()
+    );
+    expect(json).toMatchObject({
+      name: '@app/feature-client',
+      version: '0.0.0',
+      dependencies: {
+        rxjs: rootJson.dependencies.rxjs,
+      },
+    });
+  });
+
+  it('libs/feature-server', () => {
+    const json = JSON.parse(
+      readFileSync(
+        resolve(
+          __dirname,
+          '..',
+          '..',
+          '..',
+          '..',
+          'integrations/app/libs/feature/server/package.json'
+        )
+      ).toString()
+    );
+    expect(json).toMatchObject({
+      name: '@app/feature-server',
+      version: '0.0.0',
+      dependencies: {
+        rxjs: rootJson.dependencies.rxjs,
+      },
+    });
+  });
+
+  it('libs/feature-common', () => {
+    const json = JSON.parse(
+      readFileSync(
+        resolve(
+          __dirname,
+          '..',
+          '..',
+          '..',
+          '..',
+          'integrations/app/libs/feature/common/package.json'
+        )
+      ).toString()
+    );
+    expect(json).toMatchObject({
+      name: '@app/feature-common',
+      version: '0.0.0',
+      dependencies: {
+        rxjs: rootJson.dependencies.rxjs,
+      },
+    });
+  });
+
+  it('apps/client', () => {
+    const json = JSON.parse(
+      readFileSync(
+        resolve(
+          __dirname,
+          '..',
+          '..',
+          '..',
+          '..',
+          'integrations/app/apps/client/package.json'
+        )
+      ).toString()
+    );
+    expect(json).toMatchObject({
+      name: '@app/client',
+      version: '0.0.0',
+      dependencies: {
+        rxjs: rootJson.dependencies.rxjs,
+      },
+    });
+  });
+
+  it('apps/server', () => {
+    const json = JSON.parse(
+      readFileSync(
+        resolve(
+          __dirname,
+          '..',
+          '..',
+          '..',
+          '..',
+          'integrations/app/apps/server/package.json'
+        )
+      ).toString()
+    );
+    expect(json).toMatchObject({
+      name: '@app/server',
+      version: '0.0.0',
+      dependencies: {
+        rxjs: rootJson.dependencies.rxjs,
+      },
+    });
+  });
+
+  it('apps/cli', () => {
+    const json = JSON.parse(
+      readFileSync(
+        resolve(
+          __dirname,
+          '..',
+          '..',
+          '..',
+          '..',
+          'integrations/app/apps/cli/package.json'
+        )
+      ).toString()
+    );
+    expect(json).toMatchObject({
+      name: '@app/cli',
+      version: '0.0.0',
+      dependencies: {
+        rxjs: rootJson.dependencies.rxjs,
+      },
+    });
+  });
+});
