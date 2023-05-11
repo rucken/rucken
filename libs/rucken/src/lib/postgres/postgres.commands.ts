c3cc1715e3da956dc884c8b388957a5b6e242cea
Aliyev Ali <aliev@ilink.dev>
2023-05-11 12:05:26 +0000

    fix: description of flags

--- libs/rucken/src/lib/postgres/postgres.commands.ts
+++ libs/rucken/src/lib/postgres/postgres.commands.ts
@@ -26,12 +26,12 @@ export class PostgresCommands {
       {
         flags: '-a,--app-database-url [strings]',
         description:
-          'application database url used for create new database or change password of existing one (example: postgres://POSTGRES_USER:POSTGRES_PASSWORD@localhost:POSTGRES_PORT/POSTGRES_DATABASE?schema=public)',
+          'application database url used for create new database (example: postgres://POSTGRES_USER:POSTGRES_PASSWORD@localhost:POSTGRES_PORT/POSTGRES_DATABASE?schema=public)',
       },
       {
         flags: '-fu,--force-change-username [boolean]',
         description:
-          'force rename username if one exists in database excluding root (default: false)',
+          'force rename username if one exists in database for app-database-url excluding root (default: false)',
       },
       {
         flags: '-fp,--force-change-password [boolean]',
